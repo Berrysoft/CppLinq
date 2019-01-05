8b84b8b52a20afd9192b61ee279a93466239b8d0
王宇逸 <Strawberry_Str@hotmail.com>
2019-01-06 00:06:38 +0800

    Add license.

--- include/linq/string.hpp
+++ include/linq/string.hpp
@@ -1,3 +1,28 @@
+/**CppLinq string.hpp
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Berrysoft
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
 #ifndef LINQ_STRING_HPP
 #define LINQ_STRING_HPP
 
@@ -9,32 +34,19 @@ namespace linq
 {
     // SFINAE for character type
     template <typename Char>
-    struct is_char : std::false_type
-    {
-    };
+    inline constexpr bool is_char_v{ false };
 
     template <>
-    struct is_char<char> : std::true_type
-    {
-    };
+    inline constexpr bool is_char_v<char>{ true };
 
     template <>
-    struct is_char<wchar_t> : std::true_type
-    {
-    };
+    inline constexpr bool is_char_v<wchar_t>{ true };
 
     template <>
-    struct is_char<char16_t> : std::true_type
-    {
-    };
+    inline constexpr bool is_char_v<char16_t>{ true };
 
     template <>
-    struct is_char<char32_t> : std::true_type
-    {
-    };
-
-    template <typename Char>
-    constexpr bool is_char_v = is_char<Char>::value;
+    inline constexpr bool is_char_v<char32_t>{ true };
 
     // split enumerator
     namespace impl
